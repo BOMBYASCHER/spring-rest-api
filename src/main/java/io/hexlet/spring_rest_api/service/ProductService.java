9830dc73c8abd993775b9052df5a1c9b9b9ed365
Daniil <bombyascher@gmail.com>
2024-12-21 16:53:22 +0300

    Add '@Service' annotation to ProductService.

--- src/main/java/io/hexlet/spring_rest_api/service/ProductService.java
+++ src/main/java/io/hexlet/spring_rest_api/service/ProductService.java
@@ -6,9 +6,11 @@ import io.hexlet.spring_rest_api.mapper.ProductMapper;
 import io.hexlet.spring_rest_api.model.Product;
 import io.hexlet.spring_rest_api.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
