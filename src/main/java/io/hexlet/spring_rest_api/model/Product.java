96a207413e06cb56c032bac1ff0d3fb05ec819eb
Daniil <bombyascher@gmail.com>
2024-12-21 16:48:59 +0300

    Implement Lombok. Update Product entity: add getter/setter and change type of 'availability' field.

--- src/main/java/io/hexlet/spring_rest_api/model/Product.java
+++ src/main/java/io/hexlet/spring_rest_api/model/Product.java
@@ -6,6 +6,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,6 +19,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Table(name = "products")
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -31,7 +35,7 @@ public class Product {
 
     private Double rating;
 
-    private boolean availability;
+    private Boolean availability;
 
     private String image;
 
