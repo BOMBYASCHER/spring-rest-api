216f6bad6e25987e0825689bf42382e89f72486f
Daniil <bombyascher@gmail.com>
2024-12-21 17:17:55 +0300

    Add GlobalExceptionHandler to catch error when product doesn't exist - just return not found page.

---
+++ src/main/java/io/hexlet/spring_rest_api/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package io.hexlet.spring_rest_api.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException() {
+        return ResponseEntity.notFound().build();
+    }
+}
