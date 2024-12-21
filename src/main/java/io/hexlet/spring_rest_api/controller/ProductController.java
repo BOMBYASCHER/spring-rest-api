7e62b25a3f8e92d35175e3196203754df3b5b44d
Daniil <bombyascher@gmail.com>
2024-12-21 17:00:19 +0300

    Implement ProductService into ProductController.

--- src/main/java/io/hexlet/spring_rest_api/controller/ProductController.java
+++ src/main/java/io/hexlet/spring_rest_api/controller/ProductController.java
@@ -1,5 +1,10 @@
 package io.hexlet.spring_rest_api.controller;
 
+import io.hexlet.spring_rest_api.dto.ProductCUDTO;
+import io.hexlet.spring_rest_api.dto.ProductDTO;
+import io.hexlet.spring_rest_api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,33 +12,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
     @GetMapping
-    String read() {
-        return "products";
+    List<ProductDTO> read() {
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
-    String read(@PathVariable Long id) {
-        return "product: " + id;
+    ProductDTO read(@PathVariable Long id) {
+        return productService.getProductById(id);
     }
 
     @PostMapping
-    String create(@RequestBody String body) {
-        return "created: " + body;
+    @ResponseStatus(HttpStatus.CREATED)
+    ProductDTO create(@RequestBody ProductCUDTO productCreateDTO) {
+        return productService.createProduct(productCreateDTO);
     }
 
     @PutMapping("/{id}")
-    String update(@PathVariable Long id, @RequestBody String body) {
-        return "updated " + id + " to " + body;
+    ProductDTO update(@PathVariable Long id, @RequestBody ProductCUDTO productUpdateDTO) {
+        return productService.updateProduct(id, productUpdateDTO);
     }
 
     @DeleteMapping("/{id}")
-    String delete(@PathVariable Long id) {
-        return "deleted: " + id;
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable Long id) {
+        productService.deleteProduct(id);
     }
 }
