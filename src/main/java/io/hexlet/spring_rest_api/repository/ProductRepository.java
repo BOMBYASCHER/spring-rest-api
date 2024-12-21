0e73b15446ad0902ce132a1f75d2946a93298b0e
Daniil <bombyascher@gmail.com>
2024-12-21 16:19:17 +0300

    Create ProductRepository.

---
+++ src/main/java/io/hexlet/spring_rest_api/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package io.hexlet.spring_rest_api.repository;
+
+import io.hexlet.spring_rest_api.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
